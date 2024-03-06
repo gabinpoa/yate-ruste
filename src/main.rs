9b2416e34efd55fe1a4d29734fff7b0742b8ee8a
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-03-06 06:03:15 +0000

    Print text to the screen

---
+++ src/main.rs
@@ -0,0 +1,92 @@
+use core::panic;
+use std::path::Path;
+use std::time::Duration;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::{TextureCreator, WindowCanvas};
+use sdl2::surface::Surface;
+use sdl2::video::WindowContext;
+
+extern crate sdl2;
+
+fn get_text_surface(text: String) -> Surface<'static> {
+    let sdl2_tff_context = match sdl2::ttf::init() {
+        Ok(context) => context,
+        Err(error) => panic!("Following error initializing the ttf context: {:?}", error),
+    };
+
+    let font = match sdl2_tff_context.load_font(Path::new("src/JetBrainsMono-Regular.ttf"), 20) {
+        Ok(font) => font,
+        Err(error) => panic!("Following error loading font : {:?}", error),
+    };
+
+    let partial_render = font.render(&text);
+
+    let surface = match partial_render.solid(Color::WHITE) {
+        Ok(surface) => surface,
+        Err(error) => panic!("Following error solid partial rendering: {:?}", error),
+    };
+
+    surface
+}
+
+fn render_font(
+    canvas: &mut WindowCanvas,
+    texture_creator: TextureCreator<WindowContext>,
+) -> Result<(), String> {
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+    canvas.present();
+
+    let text = String::from("Hello, World!");
+
+    let text_surface = get_text_surface(text);
+    let (width, height) = (text_surface.width(), text_surface.height());
+    let text_texture = texture_creator
+        .create_texture_from_surface(text_surface)
+        .map_err(|err| err.to_string())?;
+
+    let rendering_target = Rect::new(100, 100, width, height);
+
+    let _ = canvas.copy(&text_texture, None, rendering_target)?;
+    canvas.present();
+    Ok(())
+}
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("My window demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    let texture_creator = canvas.texture_creator();
+
+    let _ = render_font(&mut canvas, texture_creator);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+    }
+}
