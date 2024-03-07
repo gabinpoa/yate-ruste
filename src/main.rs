41cc3fde23e1ff344264e59f4fed9295b13bee24
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-03-07 15:07:58 +0000

    Add padding as parameter to render_text

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn get_text_surface(text: String) -> Surface<'static> {
         Err(error) => panic!("Following error initializing the ttf context: {:?}", error),
     };
 
-    let font = match sdl2_tff_context.load_font(Path::new("src/JetBrainsMono-Regular.ttf"), 20) {
+    let font = match sdl2_tff_context.load_font(Path::new("src/JetBrainsMono-Regular.ttf"), 18) {
         Ok(font) => font,
         Err(error) => panic!("Following error loading font : {:?}", error),
     };
@@ -33,23 +33,22 @@ fn get_text_surface(text: String) -> Surface<'static> {
     surface
 }
 
-fn render_font(
+fn render_text(
     canvas: &mut WindowCanvas,
     texture_creator: TextureCreator<WindowContext>,
+    text: String,
+    padding: i32,
 ) -> Result<(), String> {
-    canvas.set_draw_color(Color::BLACK);
-    canvas.clear();
-    canvas.present();
-
-    let text = String::from("Hello, World!");
-
     let text_surface = get_text_surface(text);
     let (width, height) = (text_surface.width(), text_surface.height());
     let text_texture = texture_creator
         .create_texture_from_surface(text_surface)
         .map_err(|err| err.to_string())?;
 
-    let rendering_target = Rect::new(100, 100, width, height);
+    let rendering_target = Rect::new(padding, padding, width, height);
+
+    canvas.set_draw_color(Color::WHITE);
+    let _ = canvas.draw_rect(rendering_target);
 
     let _ = canvas.copy(&text_texture, None, rendering_target)?;
     canvas.present();
@@ -70,7 +69,16 @@ fn main() {
 
     let texture_creator = canvas.texture_creator();
 
-    let _ = render_font(&mut canvas, texture_creator);
+    canvas.set_draw_color(Color::from((20, 5, 0)));
+    canvas.clear();
+    canvas.present();
+
+    let _ = render_text(
+        &mut canvas,
+        texture_creator,
+        String::from("Hello, world"),
+        12,
+    );
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
